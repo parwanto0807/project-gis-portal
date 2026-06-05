a80e6f1fc1bcbac2a3507b7bf0eac14d7bcb4e40
Solusiit-2025 <admin-user@solusiit.id>
2026-06-06 02:40:10 +0700

    Update go-live

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,6 +8,19 @@ const withPWA = withPWAInit({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withPWA(nextConfig);
