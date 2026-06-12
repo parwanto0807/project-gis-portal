89f570f006eff6aeb1367547d2084cf24a718374
parwanto0807 <parwanto0807@gmail.com>
2026-06-12 07:39:55 +0700

    Update Icon Error

--- frontend/app/manifest.ts
+++ frontend/app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/logo-sm.png',
+        src: '/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/logo-md.png',
+        src: '/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       },
